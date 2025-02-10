f04c5f48b8e8e76f469a62199c45b366f928e23a
mahfujalarony <149530147+mahfujalarony@users.noreply.github.com>
2025-02-10 12:48:51 +0600

    7th commit

--- backend/index.js
+++ backend/index.js
@@ -36,5 +36,4 @@ app.post('/api/messages', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
